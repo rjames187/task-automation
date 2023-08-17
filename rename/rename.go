4df944144c976bb8d4e798b7e3e715b999cfcf83
rjames187 <rory.james2021@gmail.com>
2023-08-17 15:05:26 -0500

    write func for renaming

--- rename/rename.go
+++ rename/rename.go
@@ -1,1 +1,21 @@
 package rename
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func ExeRename() {
+	input := flag.String("i", "/", "Defines the path of the input for the command")
+	output := flag.String("o", "/", "Defines the path of the output for the command")
+	newName := flag.String("new", "renamed", "Defines the new name of the target files")
+
+	err := os.Rename(*input, fmt.Sprintf("%s%s", *output, *newName))
+	if err != nil {
+		fmt.Printf("Error renaming file: %s", *input)
+		os.Exit(1)
+	}
+
+	fmt.Println("File renamed successfully")
+}
